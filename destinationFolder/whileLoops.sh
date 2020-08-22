0daa9285dfabdb124b12237a1b1d95d88ebc1685
Batian Muthoga <bmuthoga@gmail.com>
2020-08-22 03:48:56 +0300

    feat: add whileLoops

---
+++ destinationFolder/whileLoops.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+number=1
+
+while [ $number -le 15 ] 
+do
+  echo "number is $number"
+  number=$((number + 4))
+done
+
+echo " "
+echo "bye"
+echo "we continue..."
+ 
+*-------##****
+3:number=1
+5:while [ $number -le 15 ] 
+7:  echo "number is $number"
+8:  number=$((number + 4))
