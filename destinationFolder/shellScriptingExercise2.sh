41c817deee0ef3ff6030c08377cbf8dd7ef00f71
Batian Muthoga <bmuthoga@gmail.com>
2020-08-22 03:55:52 +0300

    feat: add shellScriptingExercise2

---
+++ destinationFolder/shellScriptingExercise2.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Solution
+numFiles() {
+  local count=0
+
+  for i in ./*.txt
+  do
+    count=$((count + 1))
+  done
+
+  echo "Total .txt files: $count"
+}
+
+echo "program start"
+
+numFiles
+
+echo "program end"
+ 
+*-------##****
+4:numFiles() {
+17:numFiles
